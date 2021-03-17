588a7bb9892686c06237eb89194165d0d2a5c075
noefp <noeisneo@gmail.com>
2021-03-17 12:25:18 +0100

    scripts, schema and samples

---
+++ scripts/create_annot_schema.sql
@@ -0,0 +1,46 @@
+CREATE TABLE gene (
+    gene_id bigserial PRIMARY KEY,
+    gene_name varchar(80) NOT NULL,
+    genome_version varchar(80)
+);
+
+CREATE TABLE gene_gene (
+    gene_gene_id bigserial PRIMARY KEY,
+    gene_id1 bigserial REFERENCES gene(gene_id),
+    gene_id2 bigserial REFERENCES gene(gene_id)
+);
+
+CREATE TABLE annotation_type (
+    annotation_type_id bigserial PRIMARY KEY,
+    annotation_type varchar(80)
+);
+
+CREATE TABLE annotation (
+    annotation_id bigserial PRIMARY KEY,
+    annot_term varchar(80),
+    annot_desc text NOT NULL,
+    annotation_type_id bigserial REFERENCES annotation_type(annotation_type_id)
+);
+
+
+CREATE TABLE gene_annotation (
+    gene_annotation_id bigserial PRIMARY KEY,
+    gene_id bigserial REFERENCES gene(gene_id),
+    annotation_id bigserial REFERENCES annotation(annotation_id)
+);
+
+
+--
+-- Name: annotation_phonetic; Type: TABLE;  Owner: web_usr--
+
+
+GRANT ALL PRIVILEGES ON gene TO web_usr;
+GRANT ALL PRIVILEGES ON gene_gene TO web_usr;
+GRANT ALL PRIVILEGES ON annotation_type TO web_usr;
+GRANT ALL PRIVILEGES ON annotation TO web_usr;
+GRANT ALL PRIVILEGES ON gene_annotation TO web_usr;
+
+-- Index
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+CREATE INDEX annotation_idx ON annotation USING gin(annot_desc gin_trgm_ops);
+CREATE INDEX idx_lower_gname ON gene (lower(gene_name))
