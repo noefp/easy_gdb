94aaa25b63e2616e897c5a3e5e793d7c29ee94cd
noe <noeisneo@gmail.com>
2021-04-14 15:52:21 +0200

    login and about

--- toolbar.php
+++ toolbar.php
@@ -1,3 +1,9 @@
+<?php 
+if ($tb_login) {
+  include_once 'login_modal.php';
+}  
+?>
+
     <nav class="navbar navbar-expand-md bg-dark navbar-dark sticky-top" style="padding-left:10px">
       <?php 
         if ($db_logo && file_exists(realpath("$root_path/$images_path/$db_logo"))) {      
@@ -16,6 +22,9 @@
           <ul class="navbar-nav">
             <!-- <li><a href="about.php">About</a></li> -->
             <?php 
+              if ($tb_about) {
+                echo '<li class="nav-item"><a class="nav-link" href="/easy_gdb/about.php">About Us</a></li>';
+              }
               if ($tb_downloads) {
                 echo '<li class="nav-item"><a class="nav-link" href="/easy_gdb/downloads.php">Downloads</a></li>';
               }
@@ -45,17 +54,32 @@
                 ?>
               </div>
             </li>
+            
+            <?php
+              if ($tb_private) {
+                echo '<li class="nav-item"><a id="tbp_link" class="nav-link" href="#"><b>Private links</b></a></li>';
+              }
+            ?>
+            
           </ul>
         
-        <?php 
+        <?php
           if ($tb_search_box) {
-            echo '<form class="form-inline" id="egdb_search_form" action="/easy_gdb/tools/search/search_output.php" method="get">';
+            echo '<form class="ml-auto form-inline" id="egdb_search_form" action="/easy_gdb/tools/search/search_output.php" method="get">';
               echo '<input type="search_box" class="form-control mr-sm-2" id="search_box" name="search_keywords" placeholder="Search">';
-              // echo '<input type="search_box" class="form-control mr-sm-2" id="search_box" name="search_keywords" placeholder="Search">';
               echo '<button type="submit" class="btn btn-info"><i class="fa fa-search" style="font-size:16px;color:white"></i></button>';
             echo '</form>';
           }
-        ?>
+
+          if ($tb_login) {
+            // echo'<li id="login_li">
+              echo'<a id="login_link" class="ml-auto" style="color:white; cursor:pointer" data-toggle="modal" data-target="#loginModal">Log In <i class="fa fa-sign-in-alt" style="font-size:16px;color:white"></i></a>';
+              echo'<a id="logout_link" class="ml-auto" style="color:white; cursor:pointer; display:none">Log Out <i class="fa fa-sign-out-alt" style="font-size:16px;color:white"></i></a>';
+            // echo '</li>';
+          }
+       ?>
+        
+        
         </div>
 
     </nav>
@@ -66,6 +90,15 @@
        width: 193px;
        margin-right: .5rem!important;
      }
+   }
+   
+   #tbp_link {
+     display:none;
+     color:#d44;
+   }
+  
+   #tbp_link:hover {
+     color:#f44;
    }
   
 </style>
