c8121a85deefb8902b20cc1ccd8f9c357cd17751
noe <noeisneo@gmail.com>
2021-05-07 12:54:14 +0200

    new about template

--- about.php
+++ about.php
@@ -1,32 +1,34 @@
 <?php include_once realpath("header.php");?>
 
 
-<div style="max-width:900px; margin:auto">
+<!-- <div style="max-width:900px; margin:auto"> -->
   <br>
   <h1>About Us</h1>
   <div class="row">
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-      <?php include_once realpath("$custom_text_path/welcome_text.php");?>
+      <?php include_once realpath("$custom_text_path/about.php");?>
     </div>
   </div>
+
+  <?php 
+    if (file_exists(realpath("$custom_text_path/db_citation.php")) && filesize(realpath("$custom_text_path/db_citation.php")) >0) {
+      include_once realpath("$custom_text_path/db_citation.php");
+    }
+  ?>
 
   <div class="row">
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
 
       <?php 
-      if (file_exists(realpath("$custom_text_path/db_citation.php")) && filesize(realpath("$custom_text_path/db_citation.php")) >0) {
-        echo "<h4>Please, when using this web site or its data, cite us using the reference:</h4>";
-        
-        include_once realpath("$custom_text_path/db_citation.php");
-      }
-      ?>      
-
+        if ( file_exists(realpath("groups.php")) ) {
+          include_once realpath("groups.php");
+        }
+      ?>
     </div>
   </div>
 
   <br>
-  <!-- <img class="img-fluid" src=<?php echo "$images_path/header_img.png";?> /> -->
 
-</div>
+<!-- </div> -->
 
 <?php include_once realpath("$easy_gdb_path/footer.php");?>
