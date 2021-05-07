dfd1e5e808ad5a10a6af5050a1190317dde78661
noe <noeisneo@gmail.com>
2021-05-07 15:13:47 +0200

    new configuration variables added

--- templates/egdb_template_conf/easyGDB_conf.php
+++ templates/egdb_template_conf/easyGDB_conf.php
@@ -1,32 +1,56 @@
 <?php
 // File paths
-$root_path = "/var/www/html/db"; //use absolute path
+$root_path = "/var/www/html"; //use absolute path
 $easy_gdb_path = "$root_path/easy_gdb";
 $blast_dbs_path = "$root_path/blast_dbs";
 $downloads_path = "downloads";
 $images_path = "/egdb_img_samples";
 $custom_text_path = "$root_path/egdb_custom_text";
+$species_path = "$root_path/egdb_species";
+$lab_path = "$root_path/egdb_labs";
 
 // header
 $dbTitle = "DB Title";
 $header_img = "header_img.png";
 $db_logo = "egdb_logo.png";
 
+// Toolbar
+$tb_about = 1;
+$tb_downloads = 1;
+$tb_species = 1;
+$tb_search_box = 1;
+$tb_search = 1;
+$tb_blast = 1;
+$tb_jbrowse = 1;
+$tb_seq_ext = 1;
+$tb_annot_ext = 1;
+$tb_more = 1;
+
+// footer
 $logo1 = "logo1.png";
 $logo2 = "logo1.png";
 $logo3 = "logo1.png";
 $logo1_link = "https://www.google.com/";
 $logo2_link = "https://www.google.com/";
 $logo3_link = "https://www.google.com/";
-$logo1_width = 150;
-$logo2_width = 150;
-$logo3_width = 150;
+$logo1_height = 60;
+$logo2_height = 60;
+$logo3_height = 60;
 
 // Annotation
 $max_version = "1.0";
 
-//Gene example
+//Gene examples
 $gene_sample = "";
+$input_gene_list="gene1
+gene2
+gene3";
+
+// BLAST
+$blast_example=">protein_or_DNA
+ATGAGTTGTGGGGAGGGATTTATGTCACCACAAATAGAGACTAAAGGAAGTGTTGGATTC
+AAAGCGGGTGTTAAAGAGTACAAATTGATTTATTATACTCCTGAATACGAAACCAAAGAT
+ACCGATATCTTGGTAACATTTCGAGTAACTCCTCAACCTGGAGTTTCGCCTGTAGAAGCA";
 
 ?>
 
