ce1d0cce49f39f876a250c7e769febe7f416a97a
noe <noeisneo@gmail.com>
2021-05-26 12:24:14 +0200

    new path for common functions

--- custom_view.php
+++ custom_view.php
@@ -1,5 +1,5 @@
 <?php include_once realpath("header.php");?>
-<?php include_once 'common_functions.php';?>
+<?php include_once 'tools/common_functions.php';?>
 
   <br>
   <?php $custom_file = test_input($_GET["file_name"]); ?>
