780ba615fc3a2699ec442ef7104d78222db26c68
noe <noeisneo@gmail.com>
2021-07-08 20:46:54 +0200

    jbrowse installation added

--- install/setup.sh
+++ install/setup.sh
@@ -12,3 +12,9 @@ cp ../templates/blast/*fasta ../../blast_dbs/;
 cd ../../blast_dbs;
 makeblastdb -in sample_blast_DB_proteins.fasta -dbtype 'prot' -parse_seqids;
 makeblastdb -in sample_blast_DB_nucleotides.fasta -dbtype 'nucl' -parse_seqids;
+cd ../
+wget https://github.com/GMOD/jbrowse/releases/download/1.16.11-release/JBrowse-1.16.11.zip
+unzip JBrowse-1.16.11.zip
+mv JBrowse-1.16.11/ jbrowse/
+cd jbrowse/
+./setup.sh
