43e9292ff698571f423b068098c503f501f32611
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2022-03-29 18:10:01 +0200

    sequences from category folders

--- gene_seq.php
+++ gene_seq.php
@@ -8,28 +8,40 @@
 <?php
 
 $bdb_path = $blast_dbs_path;
-$sps_found = get_dir_and_files($bdb_path); // call the function
+$dir_found = get_dir_and_files($bdb_path); // call the function
 
-foreach ($sps_found as $bdb) {
+//foreach ($sps_found as $bdb) {
+foreach ($dir_found as $blast_dir) {
+	
+	if (is_dir($bdb_path.'/'.$blast_dir)) {
+		//echo "<h5>$blast_dir</h5>";
+	
+		$dbs_found = get_dir_and_files($bdb_path.'/'.$blast_dir);
+	
+		foreach ($dbs_found as $bdb) {
+			//echo "<h5>$bdb</h5>";
 
-  if ( preg_match('/\.nhr$|\.phr$/', $bdb, $match) ) {
-    $bdb = str_replace(".phr","",$bdb);
-    $bdb = str_replace(".nhr","",$bdb);
-    $full_path_db = $bdb_path."/".$bdb;
+		  if ( preg_match('/\.nhr$|\.phr$/', $bdb, $match) ) {
+		    $bdb = str_replace(".phr","",$bdb);
+		    $bdb = str_replace(".nhr","",$bdb);
+		    $full_path_db = $bdb_path.'/'.$blast_dir."/".$bdb;
 
-    exec("blastdbcmd -db {$full_path_db} -entry " . escapeshellarg($gene_name) ."| sed 's/lcl|//'" ,$ret);
+		    exec("blastdbcmd -db {$full_path_db} -entry " . escapeshellarg($gene_name) ."| sed 's/lcl|//'" ,$ret);
 
-    $blast_db = str_replace(".fasta","",$bdb);
-    $blast_db = str_replace("_"," ",$blast_db);
+		    $blast_db = str_replace(".fasta","",$bdb);
+		    $blast_db = str_replace("_"," ",$blast_db);
 
-    if ($ret) {
-      echo "<h5>$blast_db</h5>";
-      echo "<div class=\"card bg-light\">";
-      echo "<div class=\"card-body\" style=\"font-family:courier\">".implode("<br>",$ret)."</div>";
-      echo "</div><br>";
-    }
-    $ret=null;
-  }
+		    if ($ret) {
+		      echo "<h5>$blast_db</h5>";
+		      echo "<div class=\"card bg-light\">";
+		      echo "<div class=\"card-body\" style=\"font-family:courier\">".implode("<br>",$ret)."</div>";
+		      echo "</div><br>";
+		    }
+		    $ret=null;
+		  }
+	  
+		}
+	}
 
 }
 
