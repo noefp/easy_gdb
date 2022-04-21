0c7b77821eb01d0705a56766c3a6412cf741378c
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2022-04-21 17:11:15 +0200

    custom cookie text allowed

--- cookies.php
+++ cookies.php
@@ -5,11 +5,19 @@
 <div class="page_container">
 <h3>Cookies</h3>
 <br>
-  <p style="font-size:20px">
-    This site only uses functional cookies required for login control and needed cookies from third party tools such as JBrowse.
-    Jbrowse uses cookies to remember your configuration in the genome browser, such as the tracks loaded and the position in the genome.
-    When using this site you accept the use of the cookies.
-  </p>
+
+<?php
+  if ( file_exists("$custom_text_path/custom_cookies.php") ) {
+    include_once realpath("$custom_text_path/custom_cookies.php");
+  }
+  else {
+    echo "<p style=\"font-size:20px\">";
+    echo "This site only uses functional cookies required for login control and needed cookies from third party tools such as JBrowse.
+      Jbrowse uses cookies to remember your configuration in the genome browser, such as the tracks loaded and the position in the genome.
+      When using this site you accept the use of the cookies.";
+    echo "</p>";
+  }
+?>
 </div>
 
 <?php include_once realpath("$easy_gdb_path/footer.php");?>
---
+++ templates/egdb_custom_text/custom_cookies.php
@@ -0,0 +1,5 @@
+<p style="font-size:20px">
+  This site only uses functional cookies required for login control and needed cookies from third party tools such as JBrowse.
+  Jbrowse uses cookies to remember your configuration in the genome browser, such as the tracks loaded and the position in the genome.
+  When using this site you accept the use of the cookies.
+</p>
