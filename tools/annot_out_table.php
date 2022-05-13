83accdb58eeaef8ca6c03acad4a37279401d7cdf
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2022-05-13 12:55:16 +0200

    removed commented lines

--- tools/annot_out_table.php
+++ tools/annot_out_table.php
@@ -27,46 +27,14 @@ else {
 	    $annot_hash = json_decode($annot_json_file, true);
 	}
 	
-	
-  
 	// Getting all annotation types.
   $query="SELECT annotation_type_id,annotation_type from annotation_type"; // array with annotation type ids
-//  $query="SELECT annotation_type from annotation_type"; // array with annotation types
-//  $query="SELECT distinct annotation_type from annotation order by annotation_type desc"; // array with annotation types
-  // $query="SELECT distinct annotation_type from annotation where annotation_type not like 'GO %' order by annotation_type desc";
-  $res=pg_query($query) or die("Couldn't query database.");
 
-// $annot_type_hash = array();
-//
-// while ($line = pg_fetch_array($res, null, PGSQL_ASSOC)) {
-//   // echo "<p>".$line["annotation_type_id"]." ".$line["annotation_type"]."</p>";
-//   $annot_type_hash[ $line["annotation_type_id"] ] = $line["annotation_type"];
-// }
-// $annotTypeIds = array_keys($annot_type_hash);
-// $annotTypes = array_values($annot_type_hash);
+  $res=pg_query($query) or die("Couldn't query database.");
 
   $annotTypes=pg_fetch_all_columns($res);
 
-  // foreach ($annotTypes as $type) {
-  //   echo "<p>".$all_annotation_types[$type]."</p>";
-  // }
-
-
   $gNameValues=implode(",",array_map(function($input) {if(empty(trim($input))) return ""; else  return "'" . trim(pg_escape_string($input))."'" ;},$gNamesArr));
-
-
-  ////////////////////////////////////
-
-  // $query="SELECT searchValues.search_name as \"input\", array_agg( distinct (g.gene_name)) as \"genes\", array_agg(distinct (annotation.annotation_desc, annotation.annotation_type)) \"annot\"
-  // FROM
-  // gene g inner join gene_annotation on gene_annotation.gene_id=g.gene_id
-  // inner join annotation on annotation.annotation_id=gene_annotation.annotation_id
-  // right join unnest(array[{$gNameValues}]) WITH ORDINALITY AS searchValues(search_name,ord) on search_name=g.gene_name
-  // group by searchValues.search_name, searchValues.ord
-  // order by searchValues.ord asc";
-
-  //////////////////////////////////////
-
   
   $query="SELECT searchValues.search_name as \"input\", array_agg( distinct (g.gene_name)) as \"genes\", array_agg(distinct (annotation.annotation_term, annotation.annotation_desc, annotation.annotation_type_id)) \"annot\"
   FROM
@@ -107,11 +75,6 @@ else {
       return array(0=>$matches[1],1=>$matches[2],2=>$matches[3]);
     },explode(")\",\"(",substr($row["annot"],3,-3)));
 
-    // Removing \" enclosing the the multi word annotation types.
-    //array_walk($annotEntries,function(&$entry){$entry[1]=str_replace("\\\"","",$entry[1]);});
-
-//file_put_contents('php://stderr', print_r("\nSTART\n".$row["annot"]."\nSTOP\n", TRUE));
-//file_put_contents('php://stderr', print_r($annotEntries, TRUE));
 
 echo "<tr><td><a href=\"/easy_gdb/gene.php?name={$row["input"]}\" target=\"_blank\">{$row["input"]}</a></td>";
 
@@ -148,26 +111,6 @@ $annots_array = [];
 	
 }
 echo "</tr>";
-
-    // for the corresponding annotation type for the column remove quotes from descriptions and print row with terms and description
-    // $annotTerm=implode(
-    //     array_map(function($currAnnot){return $currAnnot[0];},
-    // 		        array_filter($annotEntries,function($item) use($type) {return $item[2] == $type;})),
-    //   ";");
-    
-    // // for the corresponding annotation type for the column remove quotes from descriptions and print row with terms and description
-    // $annotStr=implode(array_map(function($type) use($annotEntries) {return "<td>".
-    //   implode(
-    //     array_map(function($currAnnot){return str_replace("\\\"","",$currAnnot[1]);},
-    // 		        array_filter($annotEntries,function($item) use($type) {return $item[2] == $type;})),
-    //   ";")
-    //  ."</td>";},$annotTypes));
-			
-		// file_put_contents('php://stderr', print_r("\nSTART\n".$annotTerm."\nSTOP\n", TRUE));
-		// file_put_contents('php://stderr', print_r("\nSTART\n".$annotStr."\nSTOP\n", TRUE));
-    
-    //echo "<tr><td><a href=\"/easy_gdb/gene.php?name={$row["input"]}\" target=\"_blank\">{$row["input"]}</a></td>{$annotTerm}</tr>";
-    // echo "<tr><td><a href=\"/easy_gdb/gene.php?name={$row["input"]}\" target=\"_blank\">{$row["input"]}</a></td>{$annotTerm}{$annotStr}</tr>";
 
 	}
 	echo "</tbody></table>\n";
