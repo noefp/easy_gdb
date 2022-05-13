2398eb84ecfa41513d1ca215cbc15c2c3bbe2210
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2022-05-13 12:50:35 +0200

    added accession IDs and links for annotations

--- tools/annot_out_table.php
+++ tools/annot_out_table.php
@@ -19,10 +19,16 @@ else {
   // Get annotation types
   include_once("get_annotation_types.php");
   
-  
-  
-  
+	// load annotation links in hash
+	$annot_hash;
 
+	if ( file_exists("$annotation_links_path/annotation_links.json") ) {
+	    $annot_json_file = file_get_contents("$annotation_links_path/annotation_links.json");
+	    $annot_hash = json_decode($annot_json_file, true);
+	}
+	
+	
+  
 	// Getting all annotation types.
   $query="SELECT annotation_type_id,annotation_type from annotation_type"; // array with annotation type ids
 //  $query="SELECT annotation_type from annotation_type"; // array with annotation types
@@ -62,7 +68,7 @@ else {
   //////////////////////////////////////
 
   
-  $query="SELECT searchValues.search_name as \"input\", array_agg( distinct (g.gene_name)) as \"genes\", array_agg(distinct (annotation.annotation_desc, annotation.annotation_type_id)) \"annot\"
+  $query="SELECT searchValues.search_name as \"input\", array_agg( distinct (g.gene_name)) as \"genes\", array_agg(distinct (annotation.annotation_term, annotation.annotation_desc, annotation.annotation_type_id)) \"annot\"
   FROM
   gene g inner join gene_annotation on gene_annotation.gene_id=g.gene_id
   inner join annotation on annotation.annotation_id=gene_annotation.annotation_id
@@ -77,7 +83,8 @@ else {
   echo "<table class=\"table table-striped table-bordered\" id=\"tblResults\"><thead><tr><th>input</th>";
 
   foreach ($annotTypes as $type) {
-    echo "<th style=\"min-width:200px\">".$all_annotation_types[$type]."</th>";
+    echo "<th style=\"min-width:100px\">".$all_annotation_types[$type]." ID</th>";
+    echo "<th style=\"min-width:200px\">".$all_annotation_types[$type]." Description</th>";
   }
   // echo implode("",array_map(function($type) {return "<th style=\"min-width:200px\">{$type}</th>";},$annotTypes));
   echo "</tr></thead><tbody>";
@@ -86,32 +93,81 @@ else {
 
   // Creating Gene columns:
 
-		// Interpreting array returned by database - removing 3 characters in the end and at the beginning.
+		// Parse gene array returned by database - removing 3 characters in the end and at the beginning.
 		$geneEntries=array_map(function($geneCol) { return explode(",",$geneCol);},explode(")\",\"(",substr($row["genes"],3,-3)));
 
 		// Removing \" enclosing the the multi word gene names.
 		array_walk($geneEntries,function(&$entry) {$entry[0]=str_replace("\\\"","",$entry[0]);});
 
-		// Get all anotations for this row and creating the columns.
+		// Get all anotations for this row and create the annotation columns.
 		$annotStr="";
-    // Interpreting array returned by database - removing 3 characters in the end and at the beginning.
+    // Parse annotation array returned by database, removed 3 characters in the end and at the beginning. Saved terms, description and annotation type in $annotEntries
     $annotEntries=array_map(function($annotRow) {
-      preg_match("/(.*),([^,]*)/",$annotRow,$matches);
-      return array(0=>$matches[1],1=>$matches[2]);
+      preg_match("/([^,]*),(.+),(\d+)/",$annotRow,$matches);
+      return array(0=>$matches[1],1=>$matches[2],2=>$matches[3]);
     },explode(")\",\"(",substr($row["annot"],3,-3)));
 
     // Removing \" enclosing the the multi word annotation types.
-    array_walk($annotEntries,function(&$entry){$entry[1]=str_replace("\\\"","",$entry[1]);});
+    //array_walk($annotEntries,function(&$entry){$entry[1]=str_replace("\\\"","",$entry[1]);});
 
-    // Creating columns for each annotation filled with content if a matching annotation was found in the array.
-    $annotStr=implode(array_map(function($type) use($annotEntries) {return "<td>" .
-      implode(
-        array_map(function($currAnnot){return str_replace("\\\"","",$currAnnot[0]);},
-        array_filter($annotEntries,function($item) use($type) { return $item[1] == $type;})),
-      ";")
-    . "</td>";},$annotTypes));
+//file_put_contents('php://stderr', print_r("\nSTART\n".$row["annot"]."\nSTOP\n", TRUE));
+//file_put_contents('php://stderr', print_r($annotEntries, TRUE));
 
-    echo "<tr><td><a href=\"/easy_gdb/gene.php?name={$row["input"]}\" target=\"_blank\">{$row["input"]}</a></td>{$annotStr}</tr>";
+echo "<tr><td><a href=\"/easy_gdb/gene.php?name={$row["input"]}\" target=\"_blank\">{$row["input"]}</a></td>";
+
+foreach ($annotTypes as $type) {
+$terms_array = [];
+$annots_array = [];
+	foreach ($annotEntries as $annot_row) {
+		if ($annot_row[2] == $type) {
+			// echo "{$annot_row[0]}";
+      $q_link = "#";
+			$annot_type = $all_annotation_types[$type];
+      if ($annot_type == "TAIR10" || $annot_type == "Araport11") {
+        $annot_row[0] = preg_replace('/\.\d$/','',$annot_row[0]);
+      }
+      if ($annot_hash[$annot_type]) {
+        $q_link = $annot_hash[$annot_type];
+        $q_link = preg_replace('/query_id/',$annot_row[0],$q_link);
+      }
+
+      // $term_linked = "<a href=\"$q_link\" target=\"_blank\">$annot_row[0]</a>";
+			
+			array_push($terms_array,"<a href=\"$q_link\" target=\"_blank\">$annot_row[0]</a>");
+		}
+	}
+	echo "<td>".implode($terms_array,"; <br>")."</td><td>";
+	
+	foreach ($annotEntries as $annot_row) {
+		if ($annot_row[2] == $type) {
+			// echo "{$annot_row[1]}";
+			array_push( $annots_array, str_replace("\\\"","",$annot_row[1]) );
+		}
+	}
+	echo implode($annots_array,"; <br>")."</td>";
+	
+}
+echo "</tr>";
+
+    // for the corresponding annotation type for the column remove quotes from descriptions and print row with terms and description
+    // $annotTerm=implode(
+    //     array_map(function($currAnnot){return $currAnnot[0];},
+    // 		        array_filter($annotEntries,function($item) use($type) {return $item[2] == $type;})),
+    //   ";");
+    
+    // // for the corresponding annotation type for the column remove quotes from descriptions and print row with terms and description
+    // $annotStr=implode(array_map(function($type) use($annotEntries) {return "<td>".
+    //   implode(
+    //     array_map(function($currAnnot){return str_replace("\\\"","",$currAnnot[1]);},
+    // 		        array_filter($annotEntries,function($item) use($type) {return $item[2] == $type;})),
+    //   ";")
+    //  ."</td>";},$annotTypes));
+			
+		// file_put_contents('php://stderr', print_r("\nSTART\n".$annotTerm."\nSTOP\n", TRUE));
+		// file_put_contents('php://stderr', print_r("\nSTART\n".$annotStr."\nSTOP\n", TRUE));
+    
+    //echo "<tr><td><a href=\"/easy_gdb/gene.php?name={$row["input"]}\" target=\"_blank\">{$row["input"]}</a></td>{$annotTerm}</tr>";
+    // echo "<tr><td><a href=\"/easy_gdb/gene.php?name={$row["input"]}\" target=\"_blank\">{$row["input"]}</a></td>{$annotTerm}{$annotStr}</tr>";
 
 	}
 	echo "</tbody></table>\n";
@@ -139,24 +195,6 @@ $("#tblResults_filter").addClass("float-right");
 $("#tblResults_info").addClass("float-left");
 $("#tblResults_paginate").addClass("float-right");
   
-//   buttons:[{
-//     extend:'csv',
-//     text:'Download',
-//     title:"egdb_gene_annotation",
-//     fieldBoundary: '',
-//     fieldSeparator:"\t"
-//   },
-//   {
-//     extend:'excel',
-//     text:'Excel',
-//     title:"",
-//     fieldSeparator:"\t"
-//   },
-// 'copy'],
-// bFilter:false,
-// ordering:true,
-// select:"multi+shiftString"
-
 </script>
 
 
