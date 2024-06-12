a14e35b6c2f6c11415303219e8e0c9ab18f5617d
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2024-06-12 12:49:34 +0200

    annotation extraction using annotation file

--- tools/annot_input_list.php
+++ tools/annot_input_list.php
@@ -1,52 +1,208 @@
-<?php include realpath('../header.php'); ?>
-<br>
-<div id="dlgDownload">
-  <h3 class="text-center">Gene Annotation Search</h3>
-  <div class="form margin-20">
+<!-- HEADER -->
+<?php include_once realpath("../header.php");?>
 
-    <form id="gene_version_lookup">
-      <label for="txtGenes">Paste a list of gene IDs</label>
-      <textarea name="txtGenes" id="txtGenes" class="form-control" rows="10">
-<?php echo "$input_gene_list" ?>
-      </textarea>
-      <br>
-      <button type="submit" class="btn btn-success float-right" form="gene_version_lookup" formaction="annot_out_table.php" formmethod="post">search</button>
-    </form>
+<!-- HELP -->
+<div class="margin-20">
+  <a class="float-right" href="/easy_gdb/help/05_annotation_extraction.php"><i class='fa fa-info' style='font-size:20px;color:#229dff'></i> Help</a>
+</div>
+<br>
+<h3 class="text-center">Annotation Extraction</h3>
+
+<!-- INPUT FORM -->
+<div class="form margin-20">
+  <div style="margin:auto; max-width:900px">
+
+<?php
+  if ($file_database) {
+    echo "<form id=\"egdb_annot_file_form\" action=\"annot_output_file.php\" method=\"post\">";
+  } else {
+    echo "<form id=\"egdb_annot_file_form\" action=\"annot_output_db.php\" method=\"post\">";
+  }
+?>
+
+    <!-- FORM OPPENED -->
+    <div class="form-group">
+      <label for="search_box" style="font-size:16px">Paste a list of gene IDs</label>
+      <textarea type="search_box" class="form-control" id="annot_file_box" name="txtGenes" rows="5" style="border-color: #666"><?php echo "$input_gene_list"; ?></textarea>
+    </div>
+
+
+    <!-- IS BETTER TO SET IN ANOTHER FILE -->
+    <?php
+      if ($file_database) {
+        include_once realpath("$easy_gdb_path/tools/common_functions.php");
+
+        $all_datasets = get_dir_and_files($annotations_path); // call the function
+        asort($all_datasets);
+
+        $dir_counter = 0;
+        $data_counter = count($all_datasets);
+
+
+        foreach ($all_datasets as $annot_dataset) {
+          if (is_dir($annotations_path."/".$annot_dataset)){ // get dirs and print categories
+            $dir_counter++;
+          }
+        }
+
+        // CHECK ANNOTATION FILES
+        if ($dir_counter) {
+          echo  "<div class=\"form-group\"><label for=\"search_box\" style=\"font-size:16px\">Select your annotation/s file</label>";
+
+          foreach ($all_datasets as $dirs_and_files) {
+            if (is_dir($annotations_path."/".$dirs_and_files)){ // get dirs and print categories
+              $all_dir_datasets = get_dir_and_files($annotations_path."/".$dirs_and_files); // call the function
+              $dir_name = str_replace("_"," ",$dirs_and_files);
+              echo "<div class=\"card\" style=\"margin-bottom: 5px;\">";
+              echo "<div class=\"card-body\" style=\"widht: 100%\">";
+              echo "<h4>$dir_name</h4>";
+              echo "<div class=\"row\" style=\"margin:0px\">";
+              sort($all_dir_datasets);
+
+              foreach ($all_dir_datasets as $annot_dataset) {
+                if ( !preg_match('/\.php$/i', $annot_dataset) && !is_dir($annotations_path.'/'.$dirs_and_files.'/'.$annot_dataset) &&  !preg_match('/\.json$/i', $annot_dataset) && file_exists($annotations_path.'/'.$dirs_and_files.'/'.$annot_dataset)   ) {
+                  $data_set_name = preg_replace('/\.[a-z]{3}$/',"",$annot_dataset);
+                  $data_set_name = str_replace("_"," ",$data_set_name);
+                  echo "<div class=\"col-sm-6 col-md-4 col-lg-4\">";
+                  echo "<label class=\"form-check-label\" style=\"cursor:pointer\">";
+                  echo "<input type=\"checkbox\" class=\"form-check-input sample_checkbox\" name=\"sample_names[]\" value=\"$annotations_path/$dirs_and_files/$annot_dataset@$data_set_name\">$data_set_name";
+                  echo "</label>";
+                  echo "<br>";
+                  echo "</div>";
+                  echo "<br>";
+                }//if preg_match
+              }//foreach all_dir
+              echo "</div>";
+              echo "</div>";
+              echo "</div>";
+            }//if is_dir
+          }// foreach dir
+        }//if dir_counter
+
+        elseif ($dir_counter === 0 && $data_counter === 1) {
+          echo "<div class=\"form-group\">";
+          echo "</div>";
+        }
+
+        else {
+          echo "<div class=\"form-group\">";
+          echo "<label for=\"search_box\" style=\"font-size:16px\">Select your annotation file</label>";
+          echo "<select class=\"form-control\" name=\"sample_names[]\">";
+          foreach ($all_datasets as $dataset) {
+            $dataset_name = preg_replace('/\.[a-z]{3}$/',"",$dataset);
+            $dataset_name = str_replace("_"," ",$dataset_name);
+            echo "<option value=\"$annotations_path/$dataset@$dataset_name\">$dataset_name</option>";
+          }
+          echo "</select>";
+          echo "</div>";
+        }
+
+      }//if file_database
+    ?>
+
     <br>
+    <button type="submit" class="btn btn-info float-right" form="egdb_annot_file_form" style="margin-top: -5px" formmethod="post">Search</button>
+    <br>
+    <br>
+    <br>
+    </form>
   </div>
 </div>
 
-<?php include_once '../footer.php';?>
+
+<!-- ERROR BANNER -->
+<div class="modal fade" id="no_gene_modal" role="dialog">
+  <div class="modal-dialog modal-sm">
+
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title" style="text-align: center;">ERROR</h4>
+      </div>
+      <div class="modal-body">
+        <div style="text-align: center;">
+          <p id="annot_input_modal"></p>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+  </div>
+</div>
 
 
-<style>
-  .margin-20 {
-    margin: 20px;
+<!-- FOOTER -->
+<?php include_once realpath("$easy_gdb_path/footer.php");?>
+
+
+<!-- IS BETTER TO ADD TO THE GENERAL CSS -->
+<style>  
+  .info_icon {
+    background-color:#4387FD;
+    border-radius:20px;
+    vertical-align: top;
+    border:0px;
+    display:inline-block;
+    color:#ffffff;
+    font-family:"Georgia",Georgia,Serif;
+    font-size:12px;
+    font-weight:bold;
+    font-style:normal;
+    width:18px;
+    height:18px;
+    line-height:18px;
+    text-align:center;
+  }
+  .info_icon:hover {
+    background-color:#5EA1FF;
+    color:#0000CC;
+  }
+
+  .info_icon:active {
+    position:relative;
+    top:1px;
   }
 </style>
 
-<script>
-  $(document).ready(function () {
 
-    $('#gene_version_lookup').submit(function () {
-      var gene_lookup_input = $('#txtGenes').val();
-      var gene_count = (gene_lookup_input.match(/\n/g)||[]).length
+<!-- JAVASCRIPT -->
+<script> 
+$(document).ready(function () {
+  $('#egdb_annot_file_form').submit(function() {
+    var gene_id = $('#annot_file_box').val();
+    var data_set_selected = false;
+    var file_database = "<?php echo $file_database; ?>";
+    var select_field = $('.sample_checkbox').length > 0;
 
-      // alert("gene_lookup_input: "+gene_lookup_input+", gene_count: "+gene_count);
-
-      //check input genes from gene lookup before sending form
-      var max_input = "<?php echo $max_annotation_input ?>";
-      if (!max_input) {
-        max_input = 200;
-      }
-      
-      if (gene_count > max_input) {
-          alert("A maximum of "+max_input+" sequences can be provided as input, your input has: "+gene_count);
+    if (select_field) {
+      $('.sample_checkbox').each(function() {
+        if ($(this).is(':checked')) {
+          data_set_selected = true;
           return false;
-      }
+        }
+      });
+    }
 
+    // Forms
+    if (!gene_id) {
+      $("#annot_input_modal").html( "No input provided in the search box" );
+      $('#no_gene_modal').modal();
+      return false;
+    }
+    else if (gene_id.length < 3) {
+      $("#annot_input_modal").html( "Input is too short, please provide a longer term to search" );
+      $('#no_gene_modal').modal();
+      return false;
+    }
+    else if (file_database === '1' && !data_set_selected && select_field) {
+      $("#annot_input_modal").html( "No annotation file/s selected" );
+      $('#no_gene_modal').modal();
+      return false;
+    }
+    else {
       return true;
-    });
-
+    };
   });
+});
+
 </script>
---
+++ tools/annot_output_file.php
@@ -0,0 +1,197 @@
+<!-- HEADER -->
+<?php include_once realpath("../header.php");?> 
+
+<!-- RETURN AND HELP-->
+<div class="margin-20">
+  <a class="float-right" href="/easy_gdb/help/05_annotation_extraction.php"><i class='fa fa-info' style='font-size:20px;color:#229dff'></i> Help</a>
+</div>
+
+<a href="annot_input_list.php" class="float-left" style="text-decoration: underline;"><i class="fas fa-reply" style="color:#229dff"></i> Back to input</a>
+<br>
+
+<!-- HTML -->
+<div class="page_container">
+
+
+<!-- POST INPUT -->
+<?php
+  $gNamesArr=array_filter(explode("\n",trim($_POST["txtGenes"])),function($gName) {return ! empty($gName);});
+?>
+
+
+<!-- MAKE TEST INPUT PILLARLO DEL COMMON_FUNCTIONS (CREAR SI NO TEST INPUT FILE) -->
+<?php
+  function test_input3($data) {
+    $data = preg_replace('/[\<\>\t\r\;]+/','',$data);
+    $data = htmlspecialchars($data);
+    if ( preg_match('/\s+/',$data) ) {
+      $data_array = explode(' ',$data,99);
+      foreach ($data_array as $key=>&$value) {
+        if (strlen($value) < 3) {
+          unset($data_array[$key]);
+        }
+      }
+      $data = implode(' ',$data_array);
+    }
+    $data = stripslashes($data);
+    return $data;
+  }
+?>
+
+
+<!-- INCLUDE TABLE -->
+<?php
+  if(empty($gNamesArr)) {
+    echo "<br>";
+    echo "<h2>No genes to search provided</h2>";
+  }
+
+  else { //DECLARE THE FUNCTION  include_once realpath("annot_file.php");
+  $search_query = [];
+    foreach ($gNamesArr as $gene_name) {
+      $one_gene = test_input3($gene_name);
+      array_push($search_query, $one_gene);
+    }
+
+    function print_annot_table($desc_input, $annot_file, $annot_hash, $dataset_name, $table_counter) {
+
+      echo "<div class=\"collapse_section pointer_cursor\" data-toggle=\"collapse\" data-target=\"#Annot_table_$table_counter\" aria-expanded=\"true\"><i class=\"fas fa-sort\" style=\"color:#229dff\"></i> $dataset_name</div>";
+      $annot_file = str_replace(" ", "\\ ", $annot_file);
+
+      $head_command = "head -n 1 $annot_file";
+      $output_head = exec($head_command);
+
+
+      $grep_input = implode($desc_input,"\|");
+      $grep_command = "grep -i '$grep_input' $annot_file";
+      exec($grep_command, $output);
+
+
+      // TABLE BEGIN
+      echo "<div id=\"Annot_table_$table_counter\" class=\"collapse show\"><div class=\"data_table_frame\"><table id=\"tblAnnotations\" class=\"tblAnnotations table table-striped table-bordered\">\n";
+
+
+      // TABLE HEADER
+      echo "<thead><tr>\n";
+      $columns = explode("\t", $output_head);
+      $col_number = count($columns);
+
+      foreach ($columns as $col) {
+        echo "<th>$col</th>\n";
+      }
+      echo "</tr></thead>\n";
+
+
+      // TABLE BODY
+      echo "<tbody>\n";
+
+      foreach ($output as $line) {
+        echo "<tr>\n";
+        $data = explode("\t", $line);
+        for ($n = 0; $n <= $col_number-1; $n++) {
+          if ($data[$n]) {
+            if ($n == 0) {
+              echo "<td><a href=\"/easy_gdb/gene.php?name=$data[$n]@$annot_file\" target=\"_blank\">$data[$n]</a></td>\n";
+            }
+            else {
+              $header_name = $columns[$n];
+              if ($header_name == "TAIR10" || $header_name == "Araport11") {
+                $query_id = preg_replace(['/query_id/', '/\.\d$/'], [$data[$n], ''], $annot_hash[$header_name]);
+                echo "<td><a href=\"$query_id\" target=\"_blank\">$data[$n]</a></td>\n";
+              }
+              elseif (strpos($data[$n], ';') && $header_name == "InterPro") {
+                $ipr_data = explode(';', $data[$n]);
+                $ipr_links = '';
+                foreach ($ipr_data as $ipr_id) {
+                  $query_id = str_replace('query_id', $ipr_id, $annot_hash[$header_name]);
+                  $ipr_links .= "<a href=\"$query_id\" target=\"_blank\">$ipr_id</a>;<br>";
+                }
+                $ipr_links = rtrim($ipr_links, ';<br>');
+                echo "<td>$ipr_links</td>\n";
+              }
+              elseif (strpos($data[$n], ';') && $header_name == "Description") {
+                $data_semicolon = str_replace(';', ';'."<br>", $data[$n]);
+                echo "<td>$data_semicolon</td>\n";
+              }
+              elseif ($annot_hash[$header_name]) {
+                $query_id = str_replace('query_id', $data[$n], $annot_hash[$header_name]);
+                echo "<td><a href=\"$query_id\" target=\"_blank\">$data[$n]</a></td>\n";
+              }
+              else {
+                echo "<td>$data[$n]</td>\n";
+              }
+            }
+          }
+          else {
+            echo "<td></td>\n";
+          }
+        }
+        echo "</tr>\n";
+      }
+      echo "</tbody></table></div></div><br>\n";
+      $output = [];
+      } // TABLE END
+
+
+    // HASH ANNOTATION
+    if (file_exists("$annotation_links_path/annotation_links.json")) {
+      $annot_json_file = file_get_contents("$annotation_links_path/annotation_links.json");
+      $annotation_hash = json_decode($annot_json_file, true);
+    }
+
+
+    // COMMANDS AND PRINT
+    $table_counter = 1;
+
+    if ($_POST['sample_names']) {
+      foreach ($_POST['sample_names'] as $sample) {
+        list($annot_file,$dataset_name) = explode("@", $sample);
+        print_annot_table($search_query, $annot_file, $annotation_hash, $dataset_name, $table_counter);
+        $table_counter++;
+      }
+    } else {
+      include_once realpath("$easy_gdb_path/tools/common_functions.php");
+      $all_datasets = get_dir_and_files($annotations_path);
+      $annot_file = $annotations_path."/".$all_datasets[0];
+      $dataset_name = $all_datasets[0];
+      $dataset_name = preg_replace('/\.[a-z]{3}$/',"",$all_datasets[0]);
+      $dataset_name = str_replace("_"," ",$dataset_name);
+      print_annot_table($search_query, $annot_file, $annotation_hash, $dataset_name, $table_counter);
+    }
+  }
+?>
+<!-- END TABLE -->
+
+
+<br>
+<br>
+</div>
+<!-- END HTML -->
+
+
+<!-- JS DATATABLE -->
+<script type="text/javascript">
+  $(".tblAnnotations").dataTable({
+    dom:'Bfrtlpi',
+    "oLanguage": {
+      "sSearch": "Filter by:"
+      },
+    buttons: [
+      'copy', 'csv', 'excel',
+        {
+          extend: 'pdf',
+          orientation: 'landscape',
+          pageSize: 'LEGAL'
+        },
+      'print', 'colvis'
+      ]
+    });
+
+$(".dataTables_filter").addClass("float-right");
+$(".dataTables_info").addClass("float-left");
+$(".dataTables_paginate").addClass("float-right");
+
+</script>
+
+<!-- FOOTER -->
+<?php include_once realpath("$easy_gdb_path/footer.php");?>
