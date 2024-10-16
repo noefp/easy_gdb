0a14eae0ad561f2704c0753d25d79208eb97741f
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2024-10-16 16:07:55 +0200

    phenotype table added

--- tools/passport/02_pass_file_to_datatable.php
+++ tools/passport/02_pass_file_to_datatable.php
@@ -16,14 +16,84 @@
 <link rel="stylesheet" href="https://unpkg.com/leaflet.markercluster/dist/MarkerCluster.Default.css" />
 <script src="https://unpkg.com/leaflet.markercluster/dist/leaflet.markercluster.js"></script>
 
+<div class="margin-20">
+<a href="/easy_gdb/index.php" class="float-left" style="text-decoration: underline;"><i class="fas fa-reply" style="color:#229dff"></i> Back to input</a>
+</div>
+<br>
 
-<div class="page_container" style="margin-top:20px">
+<p id="load" style="text-align: center; margin:10px"><b>Tables Loading...</b></p>
+<div id="body" class="page_container" style="display:none">
   <h1 class="text-center"><?php echo "$pass_dir_title" ?></h1>
   <br>
-  <div class="data_table_frame">
+  <!-- <div class="data_table_frame"> -->
 
 <?php
+function print_table($header_cols,$hide_array,$unique_link,$tab_file,$file_name,$show)
+{
+  $display_select=['hide collapse','collapse show'];
+
+  $dataset_name1 = preg_replace('/\.[a-z]{3}$/',"",$file_name);
+  $dataset_name = str_replace("_"," ",$dataset_name1);
+
+  echo "<div id=ban_$dataset_name1 class=\"p-1 my-1 bg-secondary text-white collapse_section pointer_cursor \" data-toggle=\"collapse\" data-target=#$dataset_name1 aria-expanded=\"true\" style=\"text-align:center; border-radius: 5px\">";
+  echo"<i class=\"fas fa-sort\" style=\"color:#229dff\"></i><b style=\"font-size: 30px\"> $dataset_name </b><i class=\"fas fa-sort\" style=\"color:#229dff\"></i></div>";
+
+  echo"<div id=$dataset_name1 class=\"p-7 my-3 $display_select[$show] table_collapse\">";
+    echo "<div class=\"data_table_frame\">";
+      echo"<table id=\"tblAnnotations\" class=\"tblAnnotations table table-striped table-bordered\">\n";
+
+        $field_number = 0;
+
+        // //   TABLE HEADER
+        echo "<thead><tr>\n";
+
+      foreach ($header_cols as $head_index => $hcol) {
+        
+        if ( !in_array($head_index,$hide_array) ) {
+          echo "<th>$hcol</th>";
+          
+          // find column index for unique identifier that will link to accession info
+          if ($unique_link == $hcol) {
+            $field_number = $head_index;
+          }
+        } //close in_array
+      } //close foreach
+      
+      echo "</tr></thead><tbody>";
+      
+      foreach ($tab_file as $line) {
+        
+        $columns = explode("\t", $line);
+
+        echo "<tr>";
+          
+        foreach ($columns as $col_index => $col) {
+          
+          if ( !in_array($col_index,$hide_array) ) {
+            if ($col_index == $field_number) {
+              echo "<td><a href=\"03_passport_and_phenotype.php?pass_dir=$GLOBALS[pass_dir]&acc_id=$col\">$col</a></td>";
+              // echo "<td><a href=\"03_passport_and_phenotype.php?pass_dir=$pass_dir&row_num=$row_count\">$col</a></td>";
+              // echo "<td><a href=\"row_data.php?row_data=".$table_file.",".$row_count.",".($field_number-1)."\">$col</a></td>";
+            } else {  
+              echo "<td>$col</td>";
+            }
+          } // end in_array
+        } // end foreach columns
+        echo "</tr>";
+      } // end foreach lines
+
+      echo "</tbody></table>";
+      echo"</div>";
+      echo"</div>";
+  
+
+} // end passport file exist
+
+
+// _______________main__________________________________________
+
 // get info from passport.json
+
 if ( file_exists("$passport_path/$pass_dir/passport.json") ) {
   $pass_json_file = file_get_contents("$passport_path/$pass_dir/passport.json");
   $pass_hash = json_decode($pass_json_file, true);
@@ -37,7 +107,8 @@ if ( file_exists("$passport_path/$pass_dir/passport.json") ) {
   $colors_array = $pass_hash["marker_colors"];
   
 }
-  
+ 
+  // start printing table and header
   
 if ( file_exists("$passport_path/$pass_dir/$passport_file") ) {
   $tab_file = file("$passport_path/$pass_dir/$passport_file");
@@ -45,63 +116,35 @@ if ( file_exists("$passport_path/$pass_dir/$passport_file") ) {
   // get header array by columns
   $file_header = array_shift($tab_file);
   $header_cols = explode("\t", $file_header);
-  
-  $field_number = 0;
-  
-  // start printing table and header
-  echo "<table class=\"table\" id=\"tblResults\"><thead><tr>";
+  print_table($header_cols,$hide_array,$unique_link,$tab_file,$passport_file,true); 
+}
 
-  foreach ($header_cols as $head_index => $hcol) {
-    
-    if ( !in_array($head_index,$hide_array) ) {
-      echo "<th>$hcol</th>";
-      
-      // find column index for unique identifier that will link to accession info
-      if ($unique_link == $hcol) {
-        $field_number = $head_index;
-      }
-    } //close in_array
-  } //close foreach
+foreach ($phenotype_file_array as $phenotype_file) {
+if ( file_exists("$passport_path/$pass_dir/$phenotype_file") ) {
+  $tab_file = file("$passport_path/$pass_dir/$phenotype_file");
   
-  echo "</tr></thead><body>";
-  
-  foreach ($tab_file as $row_count => $line) {
-    
-    $columns = explode("\t", $line);
+  // get header array by columns
+  $file_header = array_shift($tab_file);
+  $header_cols = explode("\t", $file_header);
+  print_table($header_cols,$hide_array,$unique_link,$tab_file,$phenotype_file,false); 
+}
+}
 
-    echo "<tr>";
-      
-    foreach ($columns as $col_index => $col) {
-      
-      if ( !in_array($col_index,$hide_array) ) {
-        if ($col_index == $field_number) {
-          echo "<td><a href=\"03_passport_and_phenotype.php?pass_dir=$pass_dir&acc_id=$col\">$col</a></td>";
-          // echo "<td><a href=\"03_passport_and_phenotype.php?pass_dir=$pass_dir&row_num=$row_count\">$col</a></td>";
-          // echo "<td><a href=\"row_data.php?row_data=".$table_file.",".$row_count.",".($field_number-1)."\">$col</a></td>";
-        } else {
-          echo "<td>$col</td>";
-        }
-      } // end in_array
-    } // end foreach columns
-    echo "</tr>";
-  } // end foreach lines
-
-  echo "</body></table>";
-
-} // end passport file exist
 ?>
 
-  </div>
-</div>
+<!-- </div> -->
+
 <br>
 <br>
   <!-- CHICKPEA GERMPLASM PRINTED ON A MAP -->
-  <div class="p-1 my-1 bg-secondary text-white">
+   <!-- collapse_section pointer_cursor\" data-toggle=\"collapse\" data-target=\"#Annot_table_$table_counter\" aria-expanded=\"true\" -->
+  <div id="explore_ban" class="p-1 my-1 bg-secondary text-white collapse_section pointer_cursor" data-toggle="collapse" data-target="#explore_map" aria-expanded="true" style="text-align:center; border-radius: 5px">
     <!-- <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"> -->
-    <center><h1><b> Explore the map </b></h1></center>
+    <i class="fas fa-sort" style="color:#229dff"></i> <i class="fas fa-globe-americas" style="color:#ffff"></i><b style="font-size: 30px"> Explore the map </b><i class="fas fa-globe-americas" style="color:#ffff"></i> <i class="fas fa-sort" style="color:#229dff"></i>
     <!-- </div> -->
   </div>
-  <div class="p-7 my-3 border">
+  
+  <div id="explore_map" class="p-7 my-3 hide collapse">
     <!-- <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12"> -->
 
 <?php
@@ -110,7 +153,6 @@ if ( file_exists("$passport_path/$pass_dir/$passport_file") ) {
 $rows = [];
 
 // Get map info
-
 if ( file_exists("$passport_path/$pass_dir/$passport_file") ) {
   $tab_file = file("$passport_path/$pass_dir/$passport_file");
   
@@ -230,16 +272,6 @@ foreach ($rows as $row) {
 
 }
 
-// Convert $country_acc_map to correct format y listar acc en el Popup
-// foreach($country_acc_map as $coords => $acc_list) {
-//   list($latitude,$longitude) = explode(',', $coords);
-//   $data_map[] = [
-//     'acc' => implode(", ", $acc_list), // Listar Acc ID's
-//     'country' => $country,
-//     'latitude' => $latitude,
-//     'longitude' => $longitude,
-//   ];
-// }
 
 
 // Convert $data to JSON - to use it in Javascript part
@@ -250,14 +282,14 @@ $json_data_map = json_encode($data_map);
 $marker_path = "$images_path/map_labels/"; 
 $marker_colors_array = json_encode($colors_array);
 
-echo "<div id=\"map\" style=\"height: 600px;\"></div>"; // print the map
+// echo "</div>";
+
+echo "<div id=\"map\" style=\"height: 600px\"></div>"; // print the map
+echo "</div>";
 
 ?>
-
-    <!-- </div> -->
+  <!-- </div> -->
   </div>
-
-
 
 
 
@@ -265,31 +297,78 @@ echo "<div id=\"map\" style=\"height: 600px;\"></div>"; // print the map
 <!-- FOOTER -->
 <?php include_once realpath("$easy_gdb_path/footer.php");?>
 
-<script type="text/javascript">
-  $("#tblResults").dataTable({
-  	dom:'Bfrtlpi',
-    "oLanguage": {
-       "sSearch": "Filter by:"
-     },
-    "order": [],
-    "buttons": ['copy', 'csv', 'excel', 'pdf', 'print', 'colvis']
-  });
+
+ <!-- Cs -->
+ <style>
+  table.dataTable td,th  {
+    max-width: 500px;
+    white-space: nowrap;
+    overflow: hidden;
+    text-align: center;
+  }
   
-  $("#tblResults_filter").addClass("float-right");
-  $("#tblResults_info").addClass("float-left");
-  $("#tblResults_paginate").addClass("float-right");
-</script>
+  .td-tooltip {
+    cursor: pointer;
+  }
+
+  .collapse_section:hover{
+    text-decoration:underline
+  }
 
 
-<script>
+  
+</style>
 
+
+<!-- JS DATATABLE -->
+<script type="text/javascript">
+
+$(document).ready(function(){
+// //when data table is ready -> show the data table
+  $('#body').css("display","block");
+  $('#load').remove();
+
+  $(".tblAnnotations").dataTable({
+    dom:'Bfrtlpi',
+    "oLanguage": {
+      "sSearch": "Filter by:"
+      },
+    buttons: [
+      'copy', 'csv', 'excel',
+        {
+          extend: 'pdf',
+          orientation: 'landscape',
+          pageSize: 'LEGAL'
+        },
+      'print', 'colvis'
+      ],
+    "sScrollX": "100%",
+    "sScrollXInner": "110%",
+    "bScrollCollapse": true,
+    "drawCallback": function( settings ) {
+  },
+    });
+
+$(".dataTables_filter").addClass("float-right");
+$(".dataTables_info").addClass("float-left");
+$(".dataTables_paginate").addClass("float-right");
+
+});
+
+
+$(document).on('shown.bs.collapse', '#explore_map', function() {
+  // alert("map load");
+  draw_map();
+});
+
+
+
+
+function draw_map(){
   // MAP 
 
 // Get $data_map from PHP
 const data = <?php echo $json_data_map; ?>;
-
-
-
 
 
 // Personalized markers with different colors
@@ -320,8 +399,6 @@ function getIcon(color) {
 
 
 
-
-
 // MAP
 var map = L.map('map').setView([0, 0], 2);
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {attribution: '&copy; <a href="">OpenStreetMap</a> contributors'}).addTo(map);
@@ -347,6 +424,7 @@ data.forEach(item => {
 
 // Añadir el grupo de clusters al mapa
 map.addLayer(markers);
+}
 
 
 </script>
