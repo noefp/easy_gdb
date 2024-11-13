76596ca2943171a052c31620ff0ec4cf7640ff10
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2024-11-13 10:17:05 +0100

    new variables added

--- templates/egdb_conf/easyGDB_conf.php
+++ templates/egdb_conf/easyGDB_conf.php
@@ -4,25 +4,35 @@ $root_path = "/var/www/html"; //use absolute path
 $egdb_files_folder = "egdb_files";
 
 $easy_gdb_path = "$root_path/easy_gdb";
-$blast_dbs_path = "$root_path/blast_dbs";
-$expression_path = "$root_path/expression_data";
-$private_expression_path = "$root_path/private_expression_data";
-$downloads_path = "downloads";
-$lookup_path = "$root_path/lookup"; //from root 
 $images_path = "/$egdb_files_folder/egdb_images";
 $custom_text_path = "$root_path/$egdb_files_folder/egdb_custom_text";
+$downloads_path = "downloads";
+$blast_dbs_path = "$root_path/blast_dbs";
+$lookup_path = "$root_path/lookup"; //from root 
 $species_path = "$root_path/$egdb_files_folder/egdb_species";
 $lab_path = "$root_path/$egdb_files_folder/egdb_labs";
+
+  //Expression
+$expression_path = "$root_path/expression_data";
+$private_expression_path = "$root_path/private_expression_data";
+
+  //Annotations
 $annotation_links_path = "$root_path/$egdb_files_folder/annotations";
 $annotations_path = "$root_path/annotations";
 
-// Select 1 to store annotations in files or 0 to store annotations in a relational database.
-$file_database = 1;
+  //Passport
+$passport_path = "$root_path/passport";
+$phenotype_imgs_path = "$images_path/descriptors_imgs";
 
-// header
+// header: site title, header image and logo
 $dbTitle = "Easy GDB";
 $header_img = "header_img.png";
 $db_logo = "egdb_logo.png";
+
+//SWITCHES
+
+// Select 1 to store annotations in files or 0 to store annotations in a relational database.
+$file_database = 1;
 
 // Toolbar
 $tb_about = 1;
@@ -37,18 +47,28 @@ $tb_seq_ext = 1;
 $tb_annot_ext = 1;
 $tb_gene_expr = 1;
 $tb_lookup = 1;
-$tb_more = 0;
 $tb_custom = 0;
 $tb_enrichment = 0;
-
-// Expression Atlas
-$expr_cards = 1;
-$expr_menu = 1;
-$comparator_lookup = 0;
 
 // About
 $ab_citation = 1;
 $ab_labs = 1;
+
+// Expression Atlas
+$expr_menu = 1;
+$comparator_lookup = 0;
+
+// Expression tools order: 0 for not shown, >=1 to setup the order
+$positions=[
+  'description' => 1,
+  'cartoons' => 2,
+  'lines' => 3,
+  'cards' => 4,
+  'heatmap' => 5,
+  'replicartes' => 6,
+  'table' => 7
+];
+
 
 //Gene examples
 $gene_sample = "";
@@ -56,14 +76,12 @@ $input_gene_list="gene1.1
 gene2.1
 gene3.1";
 
-
 // Tools
 $max_lookup_input = 10000;
 $max_extract_seq_input = 10000;
 $max_blast_input = 20;
 $max_expression_input = 15;
 $max_annotation_input = 5000;
-
 
 // BLAST
 $blast_example=">protein_or_DNA
