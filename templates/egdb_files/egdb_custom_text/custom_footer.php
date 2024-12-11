2d8bac01f7c4026d2faebc8774d87c4fd61cef2d
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2024-12-11 12:41:33 +0100

    code for json reorganization

--- templates/egdb_files/egdb_custom_text/custom_footer.php
+++ templates/egdb_files/egdb_custom_text/custom_footer.php
@@ -1,7 +1,9 @@
 <?php //code to add logos
   $logos_path = $root_path."".$images_path."/logos";
       
-  $logos_json = file_get_contents($logos_path."/logos.json");
+  // $logos_json = file_get_contents($logos_path."/logos.json");
+  $logos_json = file_get_contents($json_files_path."/customization/logos.json");
+  
   // var_dump($labs_json);
   $jlogo = json_decode($logos_json, true);
   
