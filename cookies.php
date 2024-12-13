deb506b6e615c99b22c5eac6a558951df61923ba
Noe Fernandez Pozo <noefernandezpozo@MacBook-Pro-de-Noe.local>
2024-12-13 17:29:50 +0100

    changed JBrowse case

--- cookies.php
+++ cookies.php
@@ -13,7 +13,7 @@
   else {
     echo "<p style=\"font-size:20px\">";
     echo "This site only uses functional cookies required for login control and needed cookies from third party tools such as JBrowse.
-      Jbrowse uses cookies to remember your configuration in the genome browser, such as the tracks loaded and the position in the genome.
+      JBrowse uses cookies to remember your configuration in the genome browser, such as the tracks loaded and the position in the genome.
       When using this site you accept the use of the cookies.";
     echo "</p>";
   }
