4bf906fd7115df6fd42a47d966b0fff6212e7e75
Fernando Silva <fernandosilrei@gmail.com>
2025-02-06 10:46:54 +0100

    Created datable.js with common datable functionalities for all tables

---
+++ js/datatable.js
@@ -0,0 +1,200 @@
+
+// This function applies all the features that make up a table such as, filters, pagination and data download buttons in different formats.
+//function ( ids tables, ids button to select rows)
+
+function datatable(table_id,select_id) {
+
+    if ($(table_id+' thead tr').length < 2) { //if the table has already been deployed before
+  
+        //  create data table checkbox column at the first position
+        $(table_id+' thead tr:eq(0)').prepend('<th>Select</th>');
+        $(table_id+' tbody tr').each(function() {
+          $(this).prepend('<td><input type="checkbox" class="row-select"></td>');
+        });
+  
+        $(document).off('click', '.select-checkbox');
+  
+        $(document).on('click', '.select-checkbox', function() {
+          alert('Checkbox clicado!');
+  
+        });
+  
+  
+  // -------------------------------------------------------------------------------------------------------------------------------
+  // ------------ column search ----------------------------------------------------------------------------------------------------
+        $(table_id+' thead tr').clone(true).appendTo(table_id+' thead');
+    
+        $(table_id+' thead tr:eq(0) th').each(function(i) {
+          var title = $(this).text();
+          if (title !== "Select") {
+         $(this).html('<input style= "text-align:center;width:auto; border: solid 1px #808080; border-radius: 4px;" type="text" placeholder="Search ' + title + '" />');
+          $('input', this).on('keyup change', function() {
+          if (table.column(i).search() !== this.value || (table.column(i).search() == "" ) ){
+            var colIndex = table.colReorder.transpose(i);
+            if (table.column(colIndex).search() !== this.value) {
+              table
+                .column(colIndex)
+                .search(this.value)
+                .draw();
+            }       
+          }
+        });
+      } else {
+        var html_element='<button  style=" width:110px; border-radius: 4px; white-space: nowrap;   border: solid 1px #808080" class="btn btn_select_all" id="toggle-select-btn'+select_id+'">Select All</button>';
+        $(this).html(html_element); 
+      }
+    });
+  }
+    
+  // -----------------------------------------------------------------------------------------------------------------------
+  
+    
+      var table=$(table_id).DataTable({
+        dom:'Bfrtlpi',
+        "oLanguage": {
+          "sSearch": "Filter by:"
+          },
+        buttons: [
+          {
+              "extend": 'copy',
+               "exportOptions": {
+                "rows": function ( idx, data, node ) {
+                  return $(node).find('input.row-select').is(':checked');
+                }
+              },
+              action: function(e, dt, button, config) {
+                if ($(table_id+' tbody input.row-select:checked').length > 0) {
+                  $.fn.dataTable.ext.buttons.copyHtml5.action.call(this, e, dt, button, config);
+                } else {
+                  alert("Please select rows to copy.");
+                }
+              }
+            },
+            {
+              "extend": 'csv',
+              "exportOptions": {
+                "rows": function ( idx, data, node ) {
+                  return $(node).find('input.row-select').is(':checked');
+                }
+              },
+              action: function(e, dt, button, config) {
+                if ($(table_id+' tbody input.row-select:checked').length > 0) {
+                  $.fn.dataTable.ext.buttons.csvHtml5.action.call(this, e, dt, button, config);
+                } else {
+                  alert("Please select rows to export as CSV.");
+                }
+              }
+            },
+            {
+              "extend": 'excel',
+              "exportOptions": {
+                "rows": function ( idx, data, node ) {
+                  return $(node).find('input.row-select').is(':checked');
+                }
+              },
+              action: function(e, dt, button, config) {
+                if ($(table_id+' tbody input.row-select:checked').length > 0) {
+                  $.fn.dataTable.ext.buttons.excelHtml5.action.call(this, e, dt, button, config);
+                } else {
+                  alert("Please select rows to export as Excel.");
+                }
+              }
+            },
+            {
+              "extend": 'pdf',
+              "exportOptions": {
+                "rows": function ( idx, data, node ) {
+                  return $(node).find('input.row-select').is(':checked');
+                }
+              },
+              action: function(e, dt, button, config) {
+                if ($(table_id+' tbody input.row-select:checked').length > 0) {
+                  $.fn.dataTable.ext.buttons.pdfHtml5.action.call(this, e, dt, button, config);
+                } else {
+                  alert("Please select rows to export as PDF.");
+                }
+              }
+            },
+            {
+              "extend": 'print',
+              "exportOptions": {
+                "rows": function ( idx, data, node ) {
+                  return $(node).find('input.row-select').is(':checked');
+                }
+              },
+              action: function(e, dt, button, config) {
+                if ($(table_id+' tbody input.row-select:checked').length > 0) {
+                  $.fn.dataTable.ext.buttons.print.action.call(this, e, dt, button, config);
+                } else {
+                  alert("Please select rows to print.");
+                }
+              }
+            },'colvis'
+          ],
+        "sScrollX": "100%",
+        "sScrollXInner": "110%",
+        "bScrollCollapse": true,
+        retrieve: true,
+        colReorder: true,
+        "drawCallback": function( settings ) {
+      // $('#body').css("display","inline");
+      // $(".td-tooltip").tooltip();
+      $(".dataTables_filter input").css("border-radius","5px");
+  
+        $("table.dataTable tbody tr").hover(
+          function() {
+              // Al pasar el mouse
+              $(this).css("background-color", "#d1d1d1");
+          }, function() {
+              // Al retirar el mouse
+              $(this).css("background-color", "");
+          }
+        );
+      },
+    });
+  
+    $(".dataTables_filter").addClass("float-right");
+    $(".dataTables_info").addClass("float-left");
+    $(".dataTables_paginate").addClass("float-right");
+  
+  
+     // Initialize selection state
+     $(table_id+' tbody input.row-select').prop('checked', false);
+    updateToggleButton();
+    toggleExportButtons();
+  
+    $(table_id+' tbody').on('click', 'input.row-select', function() {
+      $(this).closest('tr').toggleClass('selected', this.checked);
+      toggleRowHighlighting();  
+      updateToggleButton();
+      toggleExportButtons();
+    });
+  
+    $('#toggle-select-btn'+select_id).on('click', function() {
+      var selectedCount = $(table_id+' tbody input.row-select:checked').length;
+      $(table_id+' tbody input.row-select').prop('checked', selectedCount === 0);
+      toggleRowHighlighting();
+      updateToggleButton();
+      toggleExportButtons();
+    });
+  
+    function toggleRowHighlighting() {
+      $(table_id+' tbody tr').each(function() {
+        var row = $(this);
+        var isChecked = row.find('input.row-select').is(':checked');
+        row.toggleClass('selected', isChecked);
+      });
+    }
+  
+  // ------------------------------------------------------------------------
+    function updateToggleButton() {
+      var selectedCount = $(table_id+' tbody input.row-select:checked').length;
+      $('#toggle-select-btn'+select_id).text(selectedCount > 0 ? 'Unselect All' : 'Select All');
+    }
+  // ------------------------------------------------------------------------
+    function toggleExportButtons() {
+      var selectedCount = $(table_id+' tbody input.row-select:checked').length;
+      $('.dt-button').prop('disabled', selectedCount === 0);
+    }
+  }
+  
