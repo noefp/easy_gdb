849c90c2a77da08aa2bc7bc4d78d4d60651d8b06
Enrique López Gómez <enrique.elg.93@gmail.com>
2025-04-16 15:02:24 +0200

    Update downloads.php

--- downloads.php
+++ downloads.php
@@ -1,7 +1,11 @@
 <?php include_once realpath("header.php");?>
-<br>
-<h2 class="text-center"><i class='far fa-arrow-alt-circle-down' style='color:#555'></i> Download Files</h2>
-
-<?php include_once realpath("$easy_gdb_path/tools/download_files.php");?>
-
+<br><br>
+<div>
+  <h1 class="text-center">Download Files <i class="far fa-arrow-alt-circle-down" style="color:#555"></i></h1>
+  <br>
+  <div id="gene-version-container" class="form margin-20" style="margin:auto; max-width:1200px">
+    <?php include_once realpath("$easy_gdb_path/tools/download_files.php");?>
+  </div>
+  <br>
+</div>
 <?php include_once realpath("$easy_gdb_path/footer.php");?>
