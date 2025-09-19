f91e8ffe5b6ecd65b34f0cb5231fbfa0d7bd10f6
Noe Fernandez Pozo <noefernandezpozo@macbookpro.home>
2025-09-19 12:54:45 +0200

    jbrwose frame from annotations json

--- jb_frame_file.php
+++ jb_frame_file.php
@@ -1,45 +1,63 @@
-<?php
-if ("$root_path/jbrowse/data/") {
 
-  $quoted_search = 0;
-  if (preg_match('/^".+"$/', $name)) {
-    $quoted_search = 1;
-  }
+<?php
 
   $jb_gene_name = test_input2($gene_name);
-
-  $counter = 0;
-  $jb_dataset = "easy_gdb_sample";
-
-  if ($dh = opendir("$root_path/jbrowse/data/")){
-    //iterate all files in dir
-    while ( ($file_name = readdir($dh)) !== false ){ 
-    
-      if (!preg_match('/^\./', $file_name) ) { //discard hidden files
-        if (is_dir("$root_path/jbrowse/data/".$file_name)){ // get dirs and print categories
-          $jb_dataset = $file_name;
-          $counter++;
-        }
-      }
-    
-    }
-  }
-
-  // if ($counter >1) {
-  //   echo "<p>Several JBrowse datasets were found</p>";
-  // }
+  $annot_filename = preg_replace('/.+\//','',$annot_file);
   
-  if ($counter == 1) {
+  if ( file_exists($json_files_path."/tools/annotations_conf.json") ) {
+    $ann_json_file = file_get_contents("$json_files_path/tools/annotations_conf.json");
+    $annot_hash = json_decode($ann_json_file, true);
+  }
+    
+  if ($annot_hash[$annot_filename]) {
+    $jb_link = $annot_hash[$annot_filename]["jbrowse"];
+    
+    if (preg_match('/\{gene_name\}/', $jb_link, $match)) {
+      $jb_link = preg_replace('/\{gene_name\}/',$jb_gene_name,$jb_link);
+    }
+    
     echo '<div class="collapse_section pointer_cursor" data-toggle="collapse" data-target="#jb_section" aria-expanded="true"><i class="fas fa-sort" style="color:#229dff"></i> Genome Browser</div>';
     echo '<div id="jb_section" class="collapse show">';
-  
-    echo "<a class=\"float-right jbrowse_link\" href=\"/jbrowse/?data=data%2F$jb_dataset&loc=$jb_gene_name&tracks=DNA%2Ctranscripts&highlight=\">Full screen</a>";
-    echo "<iframe class=\"jb_iframe\" src=\"/jbrowse/?data=data%2F$jb_dataset&loc=$jb_gene_name&tracks=DNA%2Ctranscripts&highlight=\" name=\"jbrowse_iframe\">";
+    echo "<a class=\"float-right jbrowse_link\" href=\"$jb_link\">Full screen</a>";
+    echo "<iframe class=\"jb_iframe\" src=\"$jb_link\" name=\"jbrowse_iframe\">";
     echo "<p>Your browser does not support iframes.</p> </iframe>";
     echo '</div>';
   }
-  
-}
+  else if ("$root_path/jbrowse/data/") {
+      
+    $counter = 0;
+    $jb_dataset = "";
+
+    if ($dh = opendir("$root_path/jbrowse/data/")){
+      //iterate all files in dir
+      while ( ($file_name = readdir($dh)) !== false ){ 
+    
+        if (!preg_match('/^\./', $file_name) ) { //discard hidden files
+          if (is_dir("$root_path/jbrowse/data/".$file_name)){ // get dirs and print categories
+            $jb_dataset = $file_name;
+            $counter++;
+          }
+        }
+          
+      } //end while
+    } //end if
+    
+    if ($counter == 1) {
+      echo '<div class="collapse_section pointer_cursor" data-toggle="collapse" data-target="#jb_section" aria-expanded="true"><i class="fas fa-sort" style="color:#229dff"></i> Genome Browser</div>';
+      echo '<div id="jb_section" class="collapse show">';
+
+      echo "<a class=\"float-right jbrowse_link\" href=\"/jbrowse/?data=data%2F$jb_dataset&loc=$jb_gene_name&tracks=DNA%2Ctranscripts&highlight=\">Full screen</a>";
+      echo "<iframe class=\"jb_iframe\" src=\"/jbrowse/?data=data%2F$jb_dataset&loc=$jb_gene_name&tracks=DNA%2Ctranscripts&highlight=\" name=\"jbrowse_iframe\">";
+      echo "<p>Your browser does not support iframes.</p> </iframe>";
+      echo '</div>';
+    }
+    
+  } // end else if
+
+  // else if ($counter >1) {
+  //   echo "<p>Several JBrowse datasets were found</p>";
+  // }
+
 ?>
 
 <style>
