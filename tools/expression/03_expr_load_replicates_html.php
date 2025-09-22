4792f1b41ee57c2edfacc3046a639092dbee1ce5
FernandoSR <fernandosilrei@gmail.com>
2025-09-22 16:20:08 +0200

    Add legend for number of replicates in scatter plot

--- tools/expression/03_expr_load_replicates_html.php
+++ tools/expression/03_expr_load_replicates_html.php
@@ -18,7 +18,28 @@
             ?>
           </select>
         </div>
+
         <div id="chart2" style="min-height: 365px;"></div>
+        <hr>
+        <label style="color: black; font-size: 12px; display: show;"><b>Replicate count:</b></label>
+        <div id="replicates_count" style="max-height: 100px; overflow-y: auto; padding:5px; display: block;">
+          <?php
+          $colors_array=["#ea5545", "#f46a9b", "#ef9b20", "#edbf33", "#ede15b", "#bdcf32", "#87bc45", "#27aeef", "#b33dc6",'#546ead',
+                          '#666','#999','#ccc','#000',"#a61101", "#c89", "#ab5700", "#798b00", "#437801", "#036aab", "#d0f", "#700982", 
+                            "#fe9989", "#f8aedf", "#ffdf64", "#cbff89", "#6befff", "#f77ffa",'#b66'];
+
+          $index=0;
+          $colors_array_length=count($colors_array);
+          //  var_dump($scatter_all_genes[$found_genes[0]]);
+              foreach ($scatter_all_genes[$found_genes[0]] as $gene) {
+                if($index==13)
+                { echo "<span class=\"badge\" style= \"color:white; background-color: $colors_array[$index]; padding:5px; margin:10px; white-space:nowrap; display: inline-block;\">".$gene['name'].": ".count($gene['data'])."</span>";}
+                else
+                {echo "<span class=\"badge\" style= \"background-color: $colors_array[$index]; padding:5px; margin:10px; white-space:nowrap; display: inline-block;\">".$gene['name'].": ".count($gene['data'])."</span>";}
+                if ($index<$colors_array_length-1) {$index++;} else {$index=0;}
+              }
+          ?>
+        </div>
       </div>
 
     </div>
