94866eacb47a285dafd4e67221409bc71c9594aa
Enrique López Gómez <enrique.elg.93@gmail.com>
2026-04-19 01:47:12 +0200

    Update about.php

--- about.php
+++ about.php
@@ -2,32 +2,21 @@
 
 
 <!-- <div style="max-width:900px; margin:auto"> -->
-  <!-- <br>
+<!-- <br>
   <h1>About Us</h1> -->
-  <div class="row">
+<div class="row">
     <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-      <?php include_once realpath("$custom_text_path/about.php");?>
+        <?php include_once realpath("$custom_text_path/about.php");?>
     </div>
-  </div>
+</div>
 
-  <?php 
+<?php 
     if ( $ab_citation && file_exists(realpath("$custom_text_path/db_citation.php")) && filesize(realpath("$custom_text_path/db_citation.php")) >0) {
       include_once realpath("$custom_text_path/db_citation.php");
     }
   ?>
 
-  <div class="row">
-    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-
-      <?php 
-        if ( $ab_labs && file_exists(realpath("groups.php")) ) {
-          include_once realpath("groups.php");
-        }
-      ?>
-    </div>
-  </div>
-
-  <br>
+<br>
 
 <!-- </div> -->
 
