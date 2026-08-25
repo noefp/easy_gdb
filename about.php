a148c87ddd0e28408d3b9e510b89843065f74dc6
FernandoSR <fernandosilrei@gmail.com>
2026-08-25 10:09:40 +0200

    Add conditions for About containers visibility

--- about.php
+++ about.php
@@ -1,26 +1,33 @@
 <?php include_once realpath("header.php");?>
 
 <div class="page_container">
-   <div class="row custom-container">
-      <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-         <?php include_once realpath("$custom_text_path/about.php");?>
-      </div>
-   </div>
-   <?php 
-      if ( $ab_citation && file_exists(realpath("$custom_text_path/db_citation.php")) && filesize(realpath("$custom_text_path/db_citation.php")) >0) {
+
+<?php if (file_exists("$custom_text_path/about.php"))
+   { 
+      echo'<div class="row custom-container">
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">';
+               include_once realpath("$custom_text_path/about.php");
+      echo '</div>
+      </div>';
+   }
+ 
+   if ( $ab_citation && file_exists(realpath("$custom_text_path/db_citation.php")) && filesize(realpath("$custom_text_path/db_citation.php")) >0) {
          include_once realpath("$custom_text_path/db_citation.php");
       }
-   ?>
+?>
          
-<div class="row custom-container">
-    <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-      <?php 
-        if ( $ab_labs && file_exists(realpath("groups.php")) ) {
-          include_once realpath("groups.php");
-        }
-      ?>
-    </div>
-  </div>
+
+<?php 
+      if ( $ab_labs && file_exists(realpath("groups.php")) ) {
+      echo '<div class="row custom-container">
+               <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">';
+                  include_once realpath("groups.php");
+      echo '</div>
+      </div>';
+
+      }
+?>
+
 <br>
 </div>
 
